6413c1cdd4cfc559a0d2d2e7c16e886c1d97be57
Sujoy Das <sujoykarnel@gmail.com>
2026-08-01 09:30:10 +0600

    feat: add shadcn with components

---
+++ app/(authGroup)/layout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AuthGroupLayout = async ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>
+      AuthGroupLayout
+      {children}
+    </div>
+  );
+};
+
+export default AuthGroupLayout;
