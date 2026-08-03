a2c1556e0ccac863e71b2865aca043aa301de265
Sujoy Das <sujoykarnel@gmail.com>
2026-08-03 09:01:34 +0600

    feat: auth and proxy done

---
+++ proxy.ts
@@ -0,0 +1,109 @@
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+import { jwtUtls } from "./utils/jwt";
+import { getNewAccessToken } from "./service/refreshToken";
+import { JwtPayload } from "jsonwebtoken";
+
+const AUTH_ROUTES = ["/login", "/register"];
+const PUBLIC_ROUTES = ["/", "/services"];
+
+export async function proxy(request: NextRequest) {
+  const pathName = request.nextUrl.pathname;
+
+    const cookieStore = await cookies();
+    
+  
+
+  let accessToken = request.cookies.get("accessToken")?.value;
+
+  const refreshToken = request.cookies.get("refreshToken")?.value;
+
+  let decodedAccessToken = accessToken
+    ? jwtUtls.verifyToken(accessToken, process.env.JWT_ACCESS_SECRET as string)
+    : null;
+  const decodedRefreshToken = refreshToken
+    ? jwtUtls.verifyToken(
+        refreshToken,
+        process.env.JWT_REFRESH_SECRET as string,
+      )
+        : null;
+    
+      
+
+  if (!decodedAccessToken && decodedRefreshToken) {
+    const result = await getNewAccessToken();
+
+    if (result.success) {
+      const newAccessToken = result.data.accessToken;
+
+      cookieStore.set("accessToken", newAccessToken, {
+        httpOnly: true,
+        maxAge: 60 * 60 * 24 * 1,
+        sameSite: "lax",
+      });
+
+      accessToken = newAccessToken;
+      decodedAccessToken = jwtUtls.verifyToken(
+        accessToken!,
+        process.env.JWT_ACCESS_SECRET as string,
+      );
+    }
+  }
+
+  let userRole = null;
+
+  if (!decodedAccessToken?.success) {
+    cookieStore.delete("accessToken");
+  }
+
+  if (decodedAccessToken?.success && decodedAccessToken.data) {
+    userRole = (decodedAccessToken.data as JwtPayload).role;
+    }
+    
+    console.log(userRole, "proxy");
+
+  // if user logged in and try to access auth route
+  if (accessToken && AUTH_ROUTES.includes(pathName)) {
+    if (userRole === "CUSTOMER") {
+      return NextResponse.redirect(new URL("/customer-dashboard", request.url));
+    } else if (userRole === "TECHNICIAN") {
+      return NextResponse.redirect(
+        new URL("/technician-dashboard", request.url),
+      );
+    } else if (userRole === "ADMIN") {
+      return NextResponse.redirect(new URL("/admin-dashboard", request.url));
+    } else {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  }
+
+  const isPublicRoute = PUBLIC_ROUTES.some(
+    (route) => pathName === route || pathName.startsWith(route + "/"),
+  );
+  const isAuthRoute = AUTH_ROUTES.some(
+    (route) => pathName === route || pathName.startsWith(route + "/"),
+  );
+
+  // authentication
+  if (!accessToken && !isPublicRoute && !isAuthRoute) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  // authorization
+  if (pathName.startsWith("/customer-dashboard") && userRole !== "CUSTOMER") {
+    return NextResponse.redirect(new URL("/not-found", request.url));
+  } else if (
+    pathName.startsWith("/technician-dashboard") &&
+    userRole !== "TECHNICIAN"
+  ) {
+    return NextResponse.redirect(new URL("/not-found", request.url));
+  } else if (pathName.startsWith("/admin-dashboard") && userRole !== "ADMIN") {
+    return NextResponse.redirect(new URL("/not-found", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/((?!api|_next/static|favicon.ico|_next/image|.*\\.png$).*)"],
+};
